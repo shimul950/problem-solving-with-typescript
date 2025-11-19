ea0eb6113dfef76b31fb4f1e1108e8e6aeafa505
Sayedul Islam <shimulmazumder950@gmail.com>
2025-11-19 15:48:55 +0600

    added some solution

---
+++ solution.ts
@@ -0,0 +1,162 @@
+//----------problem 1------------------
+const formatValue = (value:string|number|boolean) =>{
+    if(typeof(value) === 'string'){
+        return value.toUpperCase();
+    }else if(typeof(value) === 'number'){
+        return (value * 10);
+    }else if(typeof(value) === 'boolean'){
+        return !value;
+    }else{
+        return 'You have to put string, number or boolean type text'
+    }
+}
+
+// console.log(formatValue('hellow'))
+
+//---------------problem 2--------------
+
+const getLength =(value:string[] | number[] | string)=>{
+    if(typeof value === 'string'){
+        return value.length;
+    }else{
+        return value.length;
+    }
+}
+
+//-------------problem 3------------
+
+class Person {
+    name:string;
+    age:number;
+    constructor(name:string,age:number){
+       this.name = name;
+       this.age =  age; 
+    }
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}`
+    }
+}
+
+//----------------problem 4--------
+type ArrayofItem  ={
+    title:string;
+    rating:number;
+}
+
+const filterByRating =(arr:ArrayofItem[]):ArrayofItem[]=>{
+    let outputArray:any[] = []
+    arr.forEach((item)=>{
+        if(item.rating >=4){
+            outputArray.push(item);
+        }
+    })
+    return outputArray;
+}
+const books = [
+  { title: 'Book A', rating: 4.5 },
+  { title: 'Book B', rating: 3.2 },
+  { title: 'Book C', rating: 5.0 },
+];
+
+//--------------problem 5------------
+
+type setUsersInfoType ={
+    id:number;
+    name:string;
+    email:string;
+    isActive:boolean;
+}
+
+const filterActiveUsers =(value:setUsersInfoType[]):setUsersInfoType[] =>{
+    let activeUsers :any[] =[];
+    value.filter((item) => {item.isActive === true? activeUsers.push(item):''});
+    return activeUsers;
+}
+
+const users = [
+  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+
+
+//-----------------problem 6---------
+
+interface Book{
+    title:string;
+    author:string;
+    publishedYear:number;
+    isAvailable:boolean;
+}
+
+const printBookDetails =(value:Book) =>{
+    return (`Title: ${value.title}, Author: ${value.author},  Published: ${value.publishedYear}, Available: ${(value.isAvailable === true)? 'Yes' : 'No'}`)
+} 
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: false,
+};
+
+//------------problem 7----------
+type Arrays =(number| string)[];
+
+const getUniqueValues =(arr1:Arrays,arr2:Arrays):number[]=>{
+    const allValueFromBothArr = [...arr1,...arr2];
+    let allValue:number[] =[];
+    let outputValue:number[] =[];
+    allValueFromBothArr.forEach((value) =>{
+        if(typeof value === 'string'){
+            allValue.push(parseInt(value));
+        }
+        else{
+            allValue.push(value);
+        }
+    })
+    for(let i =0; i < allValue.length; i++){
+        const num = allValue[i];
+        if(!outputValue.includes(num)){
+            outputValue.push(num)
+        }
+        
+    }
+    return outputValue;
+}    
+
+const array1 = [1, 2, '3', 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+
+//--------------------problem 8-------------
+
+type Properties={
+    name:string;
+    price:number;
+    quantity:number;
+    discount?:number;
+}
+
+const calculateTotalPrice =(value:Properties[]):number =>{
+    let totalPrice = 0;
+    value.forEach((product)=> {
+        const productPrice = product.price * product.quantity;
+        if(product.discount){
+            totalPrice = totalPrice + (productPrice - productPrice * (product.discount/100));
+        }
+        else if(!product.discount){
+            totalPrice = totalPrice + productPrice;
+        }
+        
+    })
+    return totalPrice;
+}
+
+const products = [
+  { name: 'Pen', price: 10, quantity: 2 },
+  { name: 'Notebook', price: 25, quantity: 3, discount: 10 },
+  { name: 'Bag', price: 50, quantity: 1, discount: 20 },
+];
+
+console.log(calculateTotalPrice(products));
+
